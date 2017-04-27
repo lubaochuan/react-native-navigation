d22ba4360c46c8d8aa794c8462a1feee426a0729
lubaochuan <lubaochuan@gmail.com>
2017-04-27 16:18:26 -0500

    add icons to tab bar

--- app/config/router.js
+++ app/config/router.js
@@ -10,8 +10,20 @@ import Me from '../screens/Me';
 export const Tabs = TabNavigator({
   Feed: {
     screen: Feed,
+    navigationOptions: {
+      tabBar: {
+        label: 'Feed',
+        icon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
+      },
+    },
   },
   Me: {
     screen: Me,
+    navigationOptions: {
+      tabBar: {
+        label: 'Me',
+        icon: ({ tintColor }) => <Icon name="account-circle" size={35} color={tintColor} />
+      },
+    },
   },
 });
